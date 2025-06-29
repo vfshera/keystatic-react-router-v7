5055cc1eb3ed41a6a56f3f948e917a0f1690d9b9
Franklin Shera <fshera96@gmail.com>
2025-06-30 02:21:50 +0300

    add home

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,16 @@
-import { Link } from "react-router";
+import type { Route } from "./+types/_index";
+import { Link, href } from "react-router";
+import { reader } from "~/reader.server";
 
-export default function Home() {
+export async function loader() {
+  const posts = await reader.collections.posts.all();
+  return { posts };
+}
+export default function Home({ loaderData: { posts } }: Route.ComponentProps) {
   return (
     <div className="h-screen md:px-10">
       <div className="mx-auto h-full max-w-6xl">
-        <header className="flex items-center justify-between border-b border-dashed border-gray-400 pt-5 pb-4">
+        <header className="mb-5 flex items-center justify-between border-b border-dashed border-gray-400 pt-5 pb-4">
           <h1 className="text-xl font-bold">Keystatic with React Router v7</h1>
           <nav className="text-gray-600">
             <Link to="/keystatic" className="transition-colors hover:text-blue-600 hover:underline">
@@ -12,6 +18,18 @@ export default function Home() {
             </Link>
           </nav>
         </header>
+        <div>
+          <h2 className="font-semibold">Posts</h2>
+          <ul className="list-inside list-disc">
+            {posts.map((post) => (
+              <li key={post.slug}>
+                <Link to={`/posts/${post.slug}`} className="text-blue-500">
+                  {post.entry.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
